5194b2db145b01e6ca3c0be05206447d90bc1f19
kongrp <960139253@qq.com>
2016-07-10 23:52:38 +0800

    tp5教程第二遍学习代码追踪
    命名为thinkphp12

---
+++ application/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+
+class Index
+{
+    public function index()
+    {
+       //获取教师表中的所有数据
+       $teachers = Db::name('teacher')->select();
+		
+		//查看获取的数据
+		var_dump($teachers);
+    }
+}
