375088df96bfbba982597adf1026bee587a2e0a8
kongrp <960139253@qq.com>
2016-07-12 09:48:24 +0800

    3.2.3 获取教师表中所有数据并查看

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,12 +1,16 @@
 <?php
-namespace app\index\controller;
+namespace app\index\controller;//命名空间，也说明了文件所在的文件夹
 use think\Db;  //引用数据库操作类
 
+// Index既是类名，也是文件名，说明这个文件的名字为Index.php。
 class Teacher
 {
 	public function index()
 	{	
-		//获取数据表中第一条数据
-		var_dump(Db::name('teacher')->find());
+		//“获取教师表中所有数据”
+		$teachers = Db::name('teacher')->select();
+
+		//查看获取到的数据
+		var_dump($teachers);
 	}
 }
