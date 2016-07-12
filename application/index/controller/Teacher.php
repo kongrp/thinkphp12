0bd0de7c34e5d11c9366daaea709314e23189531
kongrp <960139253@qq.com>
2016-07-12 10:12:10 +0800

    useM层，调用M层
    但存在M层文件和C层文件重名问题，会报错。

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,22 +1,16 @@
 <?php
 namespace app\index\controller;//命名空间，也说明了文件所在的文件夹
-use think\Db;  //引用数据库操作类
+use app\model\Teacher;  //引用数据库操作类-教师模型
+/**
+ * 教师管理
+ */
 
 // Index既是类名，也是文件名，说明这个文件的名字为Index.php。
 class Teacher
 {
 	public function index()
 	{	
-		//“获取教师表中所有数据”
-		$teachers = Db::name('teacher')->select();
-
-		//查看获取到的第0号数据的name值
-		var_dump($teachers[0]['name']);
-
-		//如果变量的类型是字符串，还可以使用echo语句直接返回给用户
-		echo $teachers[0]['name'];
-
-		//查看获取的数据
-		return $teachers[0]['name'];
+		$Teacher = new Teacher;
+		var_dump($Teacher);
 	}
 }
