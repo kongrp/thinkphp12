d508a5a0ad8f0bef4f82d4e23c263bd408c73e57
kongrp <960139253@qq.com>
2016-07-12 10:47:54 +0800

    添加注释，并增加两种直接显示数据的方式

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -8,7 +8,9 @@ class TeacherController
 {
 	public function index()
 	{	
+		// $Teacher 首写字大写，说明它是一个对象, 更确切一些说明这是基于Teacher这个模型被我们手工实例化得到的，如果存在teacher数据表，它将对应teacher数据表。
 		$Teacher = new Teacher;
+		// $teachers 以s结尾，表示它是一个数组，数据中的每一项都是一个对象，这个对象基于Teahcer这个模型。
 		$teachers = $Teacher->select();
 
 		//获取第0个数据
@@ -17,5 +19,8 @@ class TeacherController
 		//调用上述对象的getData()方法
 		var_dump($teacher->getData('name'));
 
+		//另外两个直接显示数据的方式	
+		echo $teacher->getData('name');
+		return $teacher->getData('name');
 	}
 }
