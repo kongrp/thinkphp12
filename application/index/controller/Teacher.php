d5a39f2aa0e9e46f7e904178ce22ec6ed5d54a60
kongrp <960139253@qq.com>
2016-07-12 10:25:36 +0800

    解决重名问题-增加别名

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,16 +1,15 @@
 <?php
-namespace app\index\controller;//命名空间，也说明了文件所在的文件夹
-use app\model\Teacher;  //引用数据库操作类-教师模型
+namespace app\index\controller;  //命名空间，也说明了文件所在的文件夹
+use app\model\Teacher as SmallTeacher;  //引用数据库操作类-教师模型
 /**
  * 教师管理
  */
 
-// Index既是类名，也是文件名，说明这个文件的名字为Index.php。
 class Teacher
 {
 	public function index()
 	{	
-		$Teacher = new Teacher;
-		var_dump($Teacher);
+		$SmallTeacher = new SmallTeacher;
+		var_dump($SmallTeacher);
 	}
 }
