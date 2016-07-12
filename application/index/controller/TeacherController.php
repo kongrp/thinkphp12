7b90844437be8bbcd65429dce612441f7a582aca
kongrp <960139253@qq.com>
2016-07-12 10:38:50 +0800

    使用select()来获取数据库中的信息
    并dump（）查看结果

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -1,15 +1,16 @@
 <?php
-namespace app\index\controller;  //命名空间，也说明了文件所在的文件夹
-use app\model\Teacher as SmallTeacher;  //引用数据库操作类-教师模型
+namespace app\index\controller;//命名空间，也说明了文件所在的文件夹
+use app\model\Teacher;  //引用数据库操作类-教师模型
 /**
  * 教师管理
  */
-
 class TeacherController
 {
 	public function index()
 	{	
-		$SmallTeacher = new SmallTeacher;
-		var_dump($SmallTeacher);
+		$Teacher = new Teacher;
+		$teachers = $Teacher->select();
+
+		var_dump($teachers);
 	}
 }
