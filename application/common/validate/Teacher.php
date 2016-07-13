dea4f9723073116150ec9685289851b860a46691
kongrp <960139253@qq.com>
2016-07-13 12:33:43 +0800

    在Teacher验证类中设置字段验证规则

--- application/common/validate/Teacher.php
+++ application/common/validate/Teacher.php
@@ -1,11 +1,13 @@
 <?php
 namespace app\common\validate;
-use think\Validate;
+use think\Validate;  //引用内置验证类
 
 class Teacher extends Validate
 {
 	protected $rule = [
+		'username' => 'require|length:4,25|unique:teacher',
 		'email' => 'email',
+		'name'  => 'require|length:2,25',
 	];
 	protected $message = [
 		'email' => '邮箱格式有误',
