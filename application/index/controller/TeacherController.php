9bc0a8e1dea4f82f16b7834f315ea8ed29f44466
kongrp <960139253@qq.com>
2016-07-17 23:03:15 +0800

    3.5.7更新数据-对象方法-初步实现
    用户名过短，返回boolean
    不修改直接保存，返回int 0
    待重构--

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -107,25 +107,21 @@ class TeacherController extends Controller
 
 	public function update()
 	{
-		//接收数据
-		$teacher = input('post.');
-		$message = '更新成功';
-		
-		//将数据存入教师数据表
-		$Teacher = new Teacher;
+		// 接收数据，取要更新的关键字信息
+		$id = input('post.id');
+        
+        // 获取当前对象
+        $teacher = Teacher::get($id);
 
-		//根据状态定制提示信息
-		try
-		{
-			if(false === $Teacher->validate()->isUpdate()->save($teacher))
-			{
-				$message = '更新失败' . $Teacher->getError();
-			}
-		} catch(\Exception $e)
-		{
-			$message = '更新失败' . $e->getMessage();
-		}
-		
-		return $message;
+        // 写入要更新的数据
+        $teacher->name = input('post.name');
+		$teacher->username = input('post.username');
+		$teacher->sex = input('post.sex');
+		$teacher->email = input('post.email');
+
+        // 更新
+        var_dump($teacher->validate()->save());
+
+        return '更新成功';
 	}
 }
