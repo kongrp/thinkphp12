cb6ff04faedec2f010a132eb95ef6212b32ffd81
kongrp <960139253@qq.com>
2016-07-21 14:45:00 +0800

    4.4.4 引入M层-改写C层，只负责指挥
    C层是个指挥官，只负责指挥，不负责代码的具体实现

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -19,25 +19,11 @@ class LoginController extends Controller
 	//处理用户提交的登录数据
 	public function login()
 	{	
-		//先测试是否传入正确的信息
-		var_dump(input('post.'));
-
- 		// 验证用户名是否存在
- 		$map = array('username'=>input('post.username'));
- 		//var_dump($map);  --输出一个数组，输足内有两项内容，用户名和密码信息。
- 		$Teacher = Teacher::get($map);
- 		//var_dump($Teacher);  --输出例如张三这个对象的基本信息
-  
-        // 就像我以前说过的，我们每调用一个方法，都要非常清晰道它返回值类型是什么
-        // $Teacher要么是一个对象，要么是false。
-        if(false !== $Teacher && $Teacher->getData('password') === input('post.password'))
-        {			
-			// 用户名密码正确，将teacherId存session。
-        	session('teacherId', $Teacher->getData('id'));
-        	return $this->success('登录成功', url('Teacher/index'));
-        } else{
-        	// 用户名密码错误，跳转到登录界面。
-        	return $this->error('用户名或密码错误', url('index'));
-        }
+		if(Teacher::login(input('post.username'), input('post.password')))
+		{
+			return $this->success('登录成功', url('Teacher/index'));
+		} else{
+			return $this->success('用户名或密码错误', url('index'));
+		}
 	}
 }
