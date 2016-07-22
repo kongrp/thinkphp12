f60606242e585b93e7fe191989efad13d79ccefa
kongrp <960139253@qq.com>
2016-07-23 01:29:15 +0800

    6.4+6.5edit()-delete()-edit.html

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -39,10 +39,59 @@ class KlassController extends IndexController
 		} else{
 			return $this->success('操作成功', url('index'));
 		}
-
-
-
-		//var_dump(input('post.'));
 	}
-	
+
+	public function edit()
+		{
+			$id = input('get.id/d');
+			//获取所有的教师信息
+			$teachers = Teacher::all();
+			$this->assign('teachers', $teachers);
+
+			if(false === $Klass = Klass::get($id))
+			{
+				return $this->error('不存在id为：' . $id . '的记录');
+			}
+
+			$this->assign('Klass', $Klass);
+			return $this->fetch();
+		}
+
+		public function update()
+		{
+			$id = input('post.id/d');
+
+			// 获取传入的班级信息
+			$Klass = Klass::get($id);
+			if(false === $Klass)
+			{
+				return $this->error('系统未找到ID为' . $id . '的记录');
+			}
+
+			$Klass->name = input('post.name');
+			$Klass->teacher_id = input('post.teacher_id');
+			if(false === $Klass->validate()->save())
+			{
+            	return $this->error('更新错误：' . $Klass->getError());
+        	} else {
+            	return $this->success('操作成功', url('index'));
+        	}
+		}
+
+		public function delete()
+		{
+			$id = input('get.id/d');
+			// 获取传入的班级信息
+			$Klass = Klass::get($id);
+			if(false === $Klass)
+			{
+				return $this->error('系统未找到ID为' . $id . '的记录，删除失败');
+			}
+
+			if(false === $Klass->delete())
+			{
+				return $this->error('删除失败' . $Klass->getError());
+			}
+			return $this->success('删除成功', url('index'));
+		}	
 }
