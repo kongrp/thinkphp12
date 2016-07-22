e064d3828eee7a86a0170d7843fe369418f22634
kongrp <960139253@qq.com>
2016-07-22 23:58:32 +0800

    6.3增加数据-add()-save()-验证规则-add.html

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 use app\model\Klass;
+use app\model\Teacher;
 
 class KlassController extends IndexController
 {
@@ -17,6 +18,31 @@ class KlassController extends IndexController
 
 		//取回V层打包的数据，并返回给用户
 		return $this->fetch();
+	}
+
+	public function add()
+	{
+		//获取所有的教师信息
+		$teachers = Teacher::all();
+		$this->assign('teachers', $teachers);
+		return $this->fetch();
+	}
+
+	public function save()
+	{
+		$Klass = new Klass;
+		$Klass->name = input('post.name');
+		$Klass->teacher_id = input('post.teacher_id/d');
+		if(false === $Klass->validate()->save())
+		{
+			return $this->error('添加数据错误：' . $Klass->getError());
+		} else{
+			return $this->success('操作成功', url('index'));
+		}
+
+
+
+		//var_dump(input('post.'));
 	}
 	
 }
