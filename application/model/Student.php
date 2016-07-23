fa3b6dd91d32e8bb951606efcb7faf6cf685fdb5
kongrp <960139253@qq.com>
2016-07-23 17:54:43 +0800

    7.2.2解决创建时间显示-获取器getCreateTimeAttr()
    1970-01-01

--- application/model/Student.php
+++ application/model/Student.php
@@ -4,6 +4,11 @@ use think\Model;
 
 class Student extends Model
 {
+	 /**
+     * 输出性别的属性
+     * @return string 0男，1女
+     * @author kongrp <kongrp@yunzhiclub.com>
+     */
 	public function getSexAttr($value)
 	{
 		$status = array('0' => '男', '1' => '女');
@@ -14,5 +19,15 @@ class Student extends Model
 		} else{
 			return $status[0];
 		}
+	}
+
+	/**
+	 *获取要显示的创建时间
+     * @param  int $value 时间戳
+     * @return string  转换后的字符串
+	 */
+	public function getCreateTimeAttr($value)
+	{
+		return date('Y-m-d', $value);
 	}
 }
