6f28b4f12974b6ebcd6265f3b6bbac24bb882953
kongrp <960139253@qq.com>
2016-07-27 17:04:09 +0800

    8.3.1课程列表中显示班级名称-实现

--- application/model/Course.php
+++ application/model/Course.php
@@ -9,6 +9,8 @@ class Course extends Model
         return $this->belongsToMany('Klass',  config('database.prefix') . 'klass_course');
     }
 
+
+    //点编辑时，进行判断哪个班级是否选中
     public function getIsChecked(&$Klass)
     {
         // 对传入变量进行判断
@@ -33,6 +35,33 @@ class Course extends Model
             return false;
         } else {
             return true;
+        }
+    }
+
+    //在列表显示页，直接显示该课程对应的班级名称
+    public function getChecked(&$Klass)
+    {
+        // 对传入变量进行判断
+        if (!is_object($Klass) || get_class($Klass) !== 'app\model\Klass' )
+        {
+            throw new \Exception("param must be app\index\model\Klass", 1);
+        }
+
+        // 取课程ID
+        $courseId = (int)$this->id;
+        $klassId = $Klass->id;
+
+        // 定制查询条件
+        $map = array();
+        $map['klass_id'] = $klassId;
+        $map['course_id'] = $courseId;
+
+        // 从关联表中取信息
+        $KlassCourse = KlassCourse::get($map);
+        if ($KlassCourse)
+        {	
+            $klass = Klass::get($KlassCourse->klass_id);
+            return $klass->getData('name');
         }
     }
 
